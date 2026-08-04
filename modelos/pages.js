79b1540c38ed50c5decd0cba6467227bf593b620
ArthurLopes-12 <a2024953195@teiacoltec.org>
2026-08-04 09:55:17 -0300

    Colocou cadastro e login'

--- modelos/pages.js
+++ modelos/pages.js
@@ -6,24 +6,58 @@ import {database, tabelas} from './db.js';
 //Função para carregar as páginas
 function pages()
 {
-    //Exemplo
-    app.get('/', requireAuth, (req, res)=>{
-        res.send('Hello World')
-    });
 
-    app.get('/login', (req, res)=>{
-        res.render('login.ejs');
-    })
+    app.get('/', (req, res) => {
+        return res.render('/login');
+    });
 
     app.get('/cadastro', (req, res)=>{
         res.render('cadastro.ejs');
     })
 
-    app.post('/cadastro', (req,res)=>{
-        //adicionar usuário ao db
-        res.redirect('/login');
+   app.post('/cadastro', (req, res) => {
+       const {username, password} = req.body; //Req.body é os dados que o cliente envia para o servidor
+   
+       const user = await User.findOne({where : {username}});
+   
+       if(!user){
+           const novoUsuario = await User.create({username, password});
+           res.send('Novo usuário cadastrado.');
+           return res.redirect('/login');
+       }
+   
+       res.send('Usuário já existente.');
+   })
+
+       app.get('/login', (req, res)=>{
+        res.render('login.ejs');
     })
+   
+    app.post('/login', async (req, res) =>{ 
+    const {username, password} = req.body;
+
+    const user = await user.findOne({ where: {username}});
+
+    if(user){
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if(isValid){
+            res.session.userId = user.id;
+            res.session.username = user.username;
+          return res.redirect('/lobby');
+        }
+    }
+
+    res.send('Usuário ou senha incorretos. Tente novamente');
+});
+
+app.get('/lobby', requireAuth, (req, res) =>{ //Passa pelo require e renderiza o lobby
+    res.render('/lobby');
+})
+
 }
+
+
 
 //Não é necessário incluir o app.listen(), ele já está incluso em outro arquivo :D
 
